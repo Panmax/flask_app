d5d6d3b507ff07cb33ea4779b8fca56e927c6d76
Panmax <967168@qq.com>
2016-02-17 18:31:48 +0800

    add .idea to gitignore

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'hello world'
+    now = datetime.now()
+    return 'hello world %s' % now
 
 if __name__ == '__main__':
     app.debug = True
