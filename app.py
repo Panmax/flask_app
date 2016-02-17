782095fbd89a8df5af9f0be8d8c41f36bb9d4da4
Panmax <967168@qq.com>
2016-02-17 19:34:02 +0800

    redirect

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 from datetime import datetime
 
 app = Flask(__name__)
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     now = datetime.now()
-    return 'hello world %s' % now
+    return redirect('http://panmax.love/')
 
-# if __name__ == '__main__':
-#     app.debug = True
-#     app.run()
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
